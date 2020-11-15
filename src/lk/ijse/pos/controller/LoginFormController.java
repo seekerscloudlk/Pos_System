e290ac1ee4a7962a731eeed50b9f090662840731
seekerscloudlk <seekerscloudlkinfo@gmail.com>
2020-11-15 10:12:26 +0530

    1

--- src/lk/ijse/pos/controller/LoginFormController.java
+++ src/lk/ijse/pos/controller/LoginFormController.java
@@ -1,4 +1,21 @@
 package lk.ijse.pos.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+
+import java.io.IOException;
+
 public class LoginFormController {
+    public AnchorPane container;
+
+
+    public void btnLoginOnAction(ActionEvent actionEvent) throws IOException {
+        Stage stage = (Stage)container.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/DashBoardForm.fxml"))));
+        stage.centerOnScreen();
+    }
 }
