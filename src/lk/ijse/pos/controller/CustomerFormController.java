023c4f2efa4548785cfda8179b60e1e8c1db000e
seekerscloudlk <seekerscloudlkinfo@gmail.com>
2020-11-15 11:11:26 +0530

    2

--- src/lk/ijse/pos/controller/CustomerFormController.java
+++ src/lk/ijse/pos/controller/CustomerFormController.java
@@ -1,4 +1,24 @@
 package lk.ijse.pos.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class CustomerFormController {
+
+    public AnchorPane container;
+    public void backToHomeOnAction(ActionEvent actionEvent) {
+        Stage stage = (Stage) container.getScene().getWindow();
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(this.getClass().
+                    getResource("../view/DashBoardForm.fxml"))));
+            stage.centerOnScreen();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/lk/ijse/pos/controller/DashBoardFormController.java
+++ src/lk/ijse/pos/controller/DashBoardFormController.java
@@ -1,4 +1,38 @@
 package lk.ijse.pos.controller;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+
+import java.io.IOException;
+
 public class DashBoardFormController {
+    public AnchorPane container;
+
+    public void openCustomerForm(MouseEvent mouseEvent) {
+        openUI("CustomerForm");
+    }
+    public void openItemForm(MouseEvent mouseEvent) {
+        openUI("ItemForm");
+    }
+    public void openPlaceOrderForm(MouseEvent mouseEvent) {
+        openUI("PlaceOrderForm");
+    }
+    public void openOrderForm(MouseEvent mouseEvent) {
+        openUI("OrdersForm");
+    }
+    void openUI(String location){
+        Stage stage = (Stage) container.getScene().getWindow();
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(this.getClass().
+                    getResource("../view/"+location+ ".fxml"))));
+            stage.centerOnScreen();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
